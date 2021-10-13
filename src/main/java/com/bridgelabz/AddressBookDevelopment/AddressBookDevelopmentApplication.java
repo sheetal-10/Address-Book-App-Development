13c1cbb20ddff21703c2f611186657ebd03464f5
sheetal-10 <sheetalp257@gmail.com>
2021-10-13 08:23:08 +0530

    Added: project files from spring initializer

---
+++ src/main/java/com/bridgelabz/AddressBookDevelopment/AddressBookDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.AddressBookDevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookDevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressBookDevelopmentApplication.class, args);
+	}
+
+}
